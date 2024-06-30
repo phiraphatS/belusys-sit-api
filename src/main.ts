390e8e76c8d4840b98968b281dba146ddd939822
Phiraphat Sukkasem (Suea) <103838124+phiraphatS@users.noreply.github.com>
2024-06-30 18:56:03 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -59,8 +59,8 @@ async function bootstrap() {
   await app.listen(port);
 
   // console.log(`Application is running on port: ${port}`);
-  logger.log(`Application is running on port: ${port}`);
-  logger.log(`Application name: Belusys API`);
+  console.log(`Application is running on port     : ${port}`);
+  console.log(`Application name                   : Belusys API`);
   console.log('running...')
 }
 bootstrap();
